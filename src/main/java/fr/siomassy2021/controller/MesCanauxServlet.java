3ec8f8e2a1753ecfaf5979fab3ef947bdc7e7ca5
Michel Plasse <mplasse@free.fr>
2021-04-13 16:06:38 +0200

    Version minimale de /mescanaux (controleur appelle vue vierge)

---
+++ src/main/java/fr/siomassy2021/controller/MesCanauxServlet.java
@@ -0,0 +1,57 @@
+package fr.siomassy2021.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author michel
+ */
+@WebServlet(name = "MesCanauxServlet", urlPatterns = {"/mescanaux"})
+public class MesCanauxServlet extends HttpServlet {
+  private String VUE_CANAUX = "WEB-INF/mesCanaux.jsp";
+
+   /**
+   * Handles the HTTP <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    // afficher la JSP
+    request.getRequestDispatcher(VUE_CANAUX).forward(request, response);
+  }
+
+  /**
+   * Handles the HTTP <code>POST</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo() {
+    return "Short description";
+  }// </editor-fold>
+
+}
