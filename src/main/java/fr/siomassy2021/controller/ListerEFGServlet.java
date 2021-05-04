9a65b82ebaf58bb5e8febd427c93b7d192e998df
Anouar Berrouane <a.berrouane@hotmail.com>
2021-05-04 18:18:23 +0200

    lister EFG : controller + jsp

---
+++ src/main/java/fr/siomassy2021/controller/ListerEFGServlet.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.siomassy2021.controller;
+
+import fr.siomassy2021.model.Efg;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Administrateur
+ */
+@WebServlet(name = "ListerEFGServlet", urlPatterns = {"/efgs"})
+public class ListerEFGServlet extends HttpServlet {
+    
+    private final String VUE = "WEB-INF/EFGs.jsp";
+   
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Efg> listeEFG = new ArrayList();
+        listeEFG.add(new Efg("TP définir objectif"));
+        listeEFG.add(new Efg("TP cadrage"));
+        listeEFG.add(new Efg("TP tests acceptation"));
+        request.setAttribute("listeEFGs", listeEFG);
+        request.getRequestDispatcher(VUE).forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/WEB-INF/EFGs.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : EFGs
+    Created on : 4 mai 2021, 16:39:20
+    Author     : Administrateur
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Liste d'EFGs</h1>
+        
+        <ol>
+            <c:forEach items="${listeEFGs}" var="efg">
+                <li>${efg.intitule}</li>
+            </c:forEach>
+        </ol>
+    </body>
+</html>
