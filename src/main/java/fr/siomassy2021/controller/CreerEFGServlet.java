b5615d93735f25d623fa5dbfe94a7badc00002c0
ayoub <ayoubelbaz200@gmail.com>
2021-05-04 09:56:13 +0200

    Première Version CreerEFG

---
+++ src/main/java/fr/siomassy2021/controller/CreerEFGServlet.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.siomassy2021.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ayoub
+ */
+@WebServlet(name = "CreerEFGServlet", urlPatterns = {"/creerEFG"})
+public class CreerEFGServlet extends HttpServlet {
+    private final String VUE_FORMU = "WEB-INF/creerEFG.jsp";
+
+   /**
+   * Handles the HTTP <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    // Le member id en dur
+    int memberId = 1; 
+    // afficher la JSP
+    request.getRequestDispatcher(VUE_FORMU).forward(request, response);
+  }
+
+
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/WEB-INF/creerEFG.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : creerEFG
+    Created on : 4 mai 2021, 09:45:16
+    Author     : ayoub
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Creer un EFG</h1>
+    </body>
+</html> 
