404ce08feecca42c57ac13690b545c43daa07bfc
Carmel <louzolo_carmel@yahoo.fr>
2021-05-06 21:05:11 +0200

    servlet repondre a une question servlet afficher les reponses a une question

---
+++ src/main/java/fr/siomassy2021/controller/RepondreQuestionServlet.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.siomassy2021.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author KIMONA
+ */
+@WebServlet(name = "RepondreQuestionServlet", urlPatterns = {"/RepondreQuestion"})
+public class RepondreQuestionServlet extends HttpServlet {
+private String VUE_REPONSES = "WEB-INF/repondreQuestion.jsp";
+     
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+           request.getRequestDispatcher(VUE_REPONSES).forward(request, response); 
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
