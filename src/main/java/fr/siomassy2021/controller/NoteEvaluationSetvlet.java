4e13c2de320378beafa15da3973b77df85fd0ab5
borelibombo <ibombo.borel@outlook.fr>
2021-05-07 17:50:49 +0200

    Diagramme de sequence et servlet pour /notesEvaluations

---
+++ src/main/java/fr/siomassy2021/controller/NoteEvaluationSetvlet.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.siomassy2021.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author borelibombo
+ */
+@WebServlet(name = "NoteEvaluationSetvlet", urlPatterns = {"/NoteEvaluation"})
+public class NoteEvaluationSetvlet extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
