3687f5a349c9196350229298c8ab85532be979b2
borelibombo <ibombo.borel@outlook.fr>
2021-05-09 16:04:49 +0200

    Diagramme de sequence et liason entre le controller et la vue pour /noterEvaluation

--- src/main/java/fr/siomassy2021/controller/NoteEvaluationSetvlet.java
+++ src/main/java/fr/siomassy2021/controller/NoteEvaluationSetvlet.java
@@ -17,22 +17,17 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author borelibombo
  */
-@WebServlet(name = "NoteEvaluationSetvlet", urlPatterns = {"/NoteEvaluation"})
+@WebServlet(name = "NoteEvaluationSetvlet", urlPatterns = {"/noterEvaluation"})
 public class NoteEvaluationSetvlet extends HttpServlet {
+    
+    private final String VUE  = "/WEB-INF/noterEvaluation.jsp";
+    
 
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        request.getRequestDispatcher(VUE).forward(request, response);
 
     }
 
---
+++ src/main/webapp/WEB-INF/noterEvaluation.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : notesEvaluations
+    Created on : 7 mai 2021, 17:53:09
+    Author     : borelibombo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Note Evaluation</title>
+    </head>
+    <body>
+        <h1>NOTES</h1>
+    </body>
+</html>
