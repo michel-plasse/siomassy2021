c9e32bc96af10a466d7c408d8066410b83d933e4
borelibombo <ibombo.borel@outlook.fr>
2021-05-21 19:45:16 +0200

    finalisation de /questions

--- src/main/java/fr/siomassy2021/controller/ListerQuestionsServlet.java
+++ src/main/java/fr/siomassy2021/controller/ListerQuestionsServlet.java
@@ -6,11 +6,15 @@
 package fr.siomassy2021.controller;
 
 import fr.siomassy2021.dao.CanalDao;
+import fr.siomassy2021.dao.ListerQuestionsDao;
 import fr.siomassy2021.model.Question;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,17 +29,27 @@ import javax.servlet.http.HttpServletResponse;
 public class ListerQuestionsServlet extends HttpServlet {
 
     private final String VUE= "/WEB-INF/questions.jsp";
+    private final String VUE_ERREUR= "/WEB-INF/erreur.jsp";
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         int idCanal= 1;
+        String vue= VUE;
         // appel a la DAO 
-        List<Question> questions = CanalDao.getQuestionsByIdCanal(idCanal) ;
-        // mis en post'it
-        request.setAttribute("questions", questions);
-        // passage a la jsp
+        List<Question> questions ;
+        try {
+            questions = ListerQuestionsDao.getQuestionsByIdCanal(idCanal);
+            // mis en post'it
+            request.setAttribute("questions", questions);
+            // passage a la jsp
+        } catch (SQLException ex) {
+            Logger.getLogger(ListerQuestionsServlet.class.getName()).log(Level.SEVERE, null, ex);
+            vue=VUE_ERREUR;
+            request.setAttribute("message", ex.getMessage());
+        }
         request.getRequestDispatcher(VUE).forward(request, response);
+        
     }
 
     @Override
